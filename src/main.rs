53359fbd0f92f687c9ccc9a0869e60581c11605e
Albert Ragány-Németh <me@alb1.hu>
2023-03-26 18:22:46 +0200

    Add message box

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
-use reqwest::{header::HeaderValue, ClientBuilder};
+use reqwest::{header::HeaderValue, blocking::ClientBuilder};
+use windows::{core::*, Win32::UI::Shell::*, Win32::UI::WindowsAndMessaging::*};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let client = ClientBuilder::new().build()?;
     let response = client
         .head("http://tehetseg.inf.elte.hu/nemes-online/nemes-aktualis.html")
-        .send()
-        .await?;
+        .send()?;
     let last_modified = response.headers().get("Last-Modified");
 
     let unchanged = &HeaderValue::from_static("Mon, 13 Feb 2023 07:40:56 GMT");
-    let message = match last_modified {
-        Some(last_modified) => String::from(if last_modified == unchanged {
-            "unchanged"
+    let something = match last_modified {
+        Some(last_modified) => if last_modified == unchanged {
+            Something::Unchanged
         } else {
-            "oh?"
-        }),
-        None => response.status().to_string()
+            Something::Modified
+        },
+        None => Something::Error(response.status().to_string())
     };
 
-    println!("{}", message);
+    let message = match something {
+        Something::Unchanged => return Ok(()),
+        Something::Modified => String::from("modified"),
+        Something::Error(message) => message
+    };
+
+    let mut v: Vec<u16> = message.encode_utf16().collect();
+    v.push(0);
+    let sth = PCWSTR::from_raw(v.as_ptr());
+
+    unsafe {
+        ShellMessageBoxW(None, None, sth, w!("Title"), MB_APPLMODAL);
+    }
 
     Ok(())
+}
+
+#[derive(Debug)]
+enum Something {
+    Unchanged,
+    Modified,
+    Error(String)
 }
